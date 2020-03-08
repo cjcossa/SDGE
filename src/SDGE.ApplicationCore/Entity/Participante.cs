86da8c756f2295fa1ce4d1c2af40f6fdfccee93e
Carlos João Cossa <carloscossa62@gmail.com>
2020-03-08 22:37:33 +0200

    Add project files.

---
+++ src/SDGE.ApplicationCore/Entity/Participante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDGE.ApplicationCore.Entity
+{
+    class Participante
+    {
+        public Participante()
+        {
+
+        }
+        public int id_participante { get; set; } 
+        public string nome { get; set; } 
+        public string ocupacao { get; set; } 
+        public string instituicao { get; set; } 
+        public string email { get; set; } 
+        public string telefone { get; set; } 
+        public string titulo_academico { get; set; } 
+             
+    }
+}
